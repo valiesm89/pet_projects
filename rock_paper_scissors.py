4b8189d5ca83cdc989d279c6efa059502f2b5506
valiesm <valiesm@gmail.com>
2024-10-30 16:39:00 +0100

    almost there with a loop not working for play again

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -22,11 +22,7 @@
     #ask if you want to play again. """
 
 #dictionary
-choices = {
-    'R': 1,
-    'P': 2,
-    'S': 3
-}
+choices = ['R','P','S']
 
 
 # Functions to Build:
@@ -45,30 +41,74 @@ def get_user_choice():
             else:
                  print(f'Thanks, you selected {choice}!\n Let the battle begin!')
                  break
-        print(f'Thanks, you selected {choice}, which corresponds to {choices[choice]}!\n Let the battle begin!')
-        return choice, choices[choice]  # Return the choice and its corresponding value
+        print(f'Thanks, you selected {choice}!\n Let the battle begin!')
+        return choice
     
-get_user_choice()    
-
     # get_computer_choice(): Randomly assign a choice to the computer (using random.choice() on a list of ["rock", "paper", "scissors"]).
 import random
 
 def get_computer_choice():
-     comp_choice = random.randint(1,3)
+     comp_choice = random.choice(choices)
      return comp_choice
-
-get_computer_choice()
-    
+   
 
     # determine_winner(user, computer): Compare user and computer choices to determine the result, returning "win", "lose", or "tie".
+
+def determine_winner(user, computer):
+    if user == 'R' and computer == 'S':
+        print(f'You Won with your choice: {user} against computer choice: {computer}')
+    elif user == 'P' and computer == 'R':
+        print(f'You Won with your choice: {user} against computer choice: {computer}')
+    elif user == 'S' and computer == 'P':
+        print(f'You Won with your choice: {user} against computer choice: {computer}')
+    elif user == computer:
+        print('You guys tied with your choice: {user} against computer choice: {computer}!!!')
+    else:
+        print(f'You lost with your choice: {user} against computer choice: {computer}')
+
+    return user, computer
+
     # display_result(winner): Show the outcome based on the result from determine_winner().
+    ##skip for now as I built the logic into the above but I would write win, lose, tie and fix that to a variable such as result
+    ##Then I would call the result, user and computer to the display_result function
+     
+
     # play_again(): Prompt to check if the user wants to play another round.
+def play_again():
+    while True:
+        answer = input('Do you want to play again? "Y" or "N"? \n').upper() == 'N'
+        if input('Do you want to play again? "Y" or "N"? \n').upper() == 'Y':
+            return True
+        elif answer == 'N':
+            return False
+        else:
+            print('You did not enter a valid value')
+            
+             
+        # # # # #more elegant from chatgpt
+        # # # # #      # play_again(): Prompt to check if the user wants to play again.
+        # # # # # def play_again():
+        # # # # #     return input('Do you want to play again? "Y" or "N"? \n').upper() == 'Y'
+
+
+
 
 # Game Loop:
 
-    # Set up a while loop that calls these functions in the correct order:
+while True:
     # Get user input.
+    user_choice = get_user_choice()
+
     # Generate computer choice.
+    computer_choice = get_computer_choice()
+
     # Check who won using determine_winner().
+    determine_winner(user_choice, computer_choice)
+
     # Display the outcome.
-    # Ask if the player wants to continue; if not, break the loop.
+    ##Skipped for now as it is integrated in determine_winner()
+
+    #check if player wants to continue
+    if not play_again():
+        print('Thanks for playing! See you next time.')
+        break    
