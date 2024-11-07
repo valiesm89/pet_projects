f50526be227b43e2ec5974967c21d4ce0279a279
valiesm <valiesm@gmail.com>
2024-11-07 14:57:35 +0100

    polished a bit and added subtitles

--- Game/GUI_edi.py
+++ Game/GUI_edi.py
@@ -8,6 +8,7 @@ choices = ['rock','paper','scissor']
 from tkinter import *
 import random
 from PIL import ImageTk, Image
+from tkinter import font
 
 # create an instance of Tk and assign it to root
 root = Tk()
@@ -64,6 +65,25 @@ sHbutton = ''
 def play():
     global rHbutton, pHbutton,sHbutton,guessp, guessc, equal
 
+    # define custom font
+    custom_font = font.Font(family="Helvetica", size=16, weight="bold")
+
+    # create title label
+    title_label = Label(root, text="Pick: Rock, Paper or Scissor", font=custom_font, fg="blue")
+    title_label.grid(row=0, column=0, columnspan=3, pady=10)
+
+    # create subtitle label
+    subtitle_label = Label(root, text="Your choice vs. Computer choice:", font=custom_font, fg="blue")
+    subtitle_label.grid(row=2, column=0, columnspan=3, sticky=W, padx=10)
+
+    # create subtitle label
+    subtitle_label = Label(root, text="Emma & Edi", font=custom_font, fg="blue")
+    subtitle_label.grid(row=4, column=0, columnspan=3, sticky=W, padx=10)
+
+    # create subtitle label
+    subtitle_label = Label(root, text="Computer", font=custom_font, fg="green")
+    subtitle_label.grid(row=4, column=2, columnspan=3, sticky=W, padx=10)
+
     rHbutton = Button(root,image= rock, command = lambda:get_user_choice('rock'))
     pHbutton = Button(root,image= paper, command = lambda:get_user_choice('paper'))
     sHbutton = Button(root,image= scissor, command = lambda:get_user_choice('scissor'))
@@ -71,13 +91,13 @@ def play():
 
     exitbutton = Button(root, text= 'Exit Game!',height=10, width=20, fg= 'red', command=root.quit)
 
-    rHbutton.grid(row= 0,column=0)
-    pHbutton.grid(row=0, column=1)
-    sHbutton.grid(row=0, column=2)
+    rHbutton.grid(row= 1,column=0)
+    pHbutton.grid(row=1, column=1)
+    sHbutton.grid(row=1, column=2)
     
-    guessp.grid(row =2,column=0)
-    comparison.grid(row=2, column=1)
-    guessc.grid(row =2,column=2)
+    guessp.grid(row =3,column=0)
+    comparison.grid(row=3, column=1)
+    guessc.grid(row =3,column=2)
     
     exitbutton.grid(row=4, column=1)
     
