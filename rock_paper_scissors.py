225e6e5ac662d741e9aeb8ffea8cee715243b4dc
valiesm <valiesm@gmail.com>
2024-11-07 09:04:44 +0100

    cleaned up the code and integrated names

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,45 +1,34 @@
-'''This is my rock paper scissors game'''
+'''This is my rock paper scissors game for Emma and Edi'''
 
-
-""" ##here I will build the functions
-
-
-
-
-##here is the main game, where I will call the functions
-
-#while loop for running game
-
-
-    #ask for input from user
-
-    #assign selection to computer
-
-    #check if win
-
-    #print result
-
-    #ask if you want to play again. """
-
-#dictionary
+# dictionary
 choices = ['R','P','S']
 
 
-# Functions to Build:
+# functions to Build:
 
-    # get_user_choice(): Prompt the user to input their choice (rock, paper, or scissors).
-
+# get_user_choice(): Prompt the user to input their choice (rock, paper, or scissors).
 def get_user_choice():
+        player_name = ''
         choice = ''
 
         while True:
-            choice = input('Please enter your choice: R, P or S: \n').upper()
+            # first deal with asking for the player
+            player_name = input('Who is playing? Please enter "Emma" or "Edi": \n').lower()
             
+            if player_name not in ['emma', 'edi']:
+                print('You are not allowed to play. This game is only for Emma and Edi!')
+                continue
+            else:
+                print(f'Hi {player_name.capitalize()}, good luck!')
+            
+            # second ask for their choice    
+            choice = input('Please enter your choice: R, P or S: \n').upper()
+
             if choice not in ['R', 'P', 'S']:
                 print('You did not enter a valid selection! Please enter R, P, S')
                 continue
             else:
-                 print(f'Thanks, you selected {choice}!\nLet the battle begin!')
+                 print(f'Thanks, you selected {choice}! yLet the battle begin!')
                  break
         return choice
     
@@ -49,10 +38,8 @@ import random
 def get_computer_choice():
      comp_choice = random.choice(choices)
      return comp_choice
-   
 
-    # determine_winner(user, computer): Compare user and computer choices to determine the result, returning "win", "lose", or "tie".
-
+# determine_winner(user, computer): Compare user and computer choices to determine the result, returning "win", "lose", or "tie".
 def determine_winner(user, computer):
     if user == 'R' and computer == 'S':
         print(f'You Won with your choice: {user} against computer choice: {computer}')
@@ -67,12 +54,7 @@ def determine_winner(user, computer):
 
     return user, computer
 
-    # display_result(winner): Show the outcome based on the result from determine_winner().
-    ##skip for now as I built the logic into the above but I would write win, lose, tie and fix that to a variable such as result
-    ##Then I would call the result, user and computer to the display_result function
-     
-
-    # play_again(): Prompt to check if the user wants to play another round.
+# play_again(): Prompt to check if the user wants to play another round.
 def play_again():
     while True:
         answer = input('Do you want to play again? "Y" or "N"? \n').upper()
@@ -82,18 +64,9 @@ def play_again():
             return False
         else:
             print('You did not enter a valid value')
-            
-             
-        # # # # #more elegant from chatgpt
-        # # # # #      # play_again(): Prompt to check if the user wants to play again.
-        # # # # # def play_again():
-        # # # # #     return input('Do you want to play again? "Y" or "N"? \n').upper() == 'Y'
-
-
 
 
 # Game Loop:
-
 while True:
     # Get user input.
     user_choice = get_user_choice()
